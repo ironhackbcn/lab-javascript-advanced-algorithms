96e9e4c2272880ada4da83ee06d301c19368304f
Juan Vicente Vazquez G <juanvicentevazquezg@gmail.com>
2019-06-21 07:18:32 +0200

    Stacks finished

--- src/StackDataStructure.js
+++ src/StackDataStructure.js
@@ -1,2 +1,27 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+  }
+  isEmpty() {
+    if (this.stackControl.length === 0) {return true;}
+    else return false;
+  }
+  canPush() {
+    if (this.stackControl.length < this.MAX_SIZE) return true;
+    else return false;
+  }
+  push(elementToAdd) {
+    if (this.canPush()) {
+      this.stackControl.push(elementToAdd);
+      return this.stackControl;
+    }else {
+        return 'Stack Overflow';
+    }
+      
+  }
+  pop() {
+      if (this.isEmpty()) {return 'Stack Underflow';}
+     else return this.stackControl.pop();
+  }
 }
